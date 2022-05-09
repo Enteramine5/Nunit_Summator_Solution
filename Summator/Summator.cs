bb025891f1bf17c367f33ed3c626e56ec1cbc4d7
Enteramine5 <105229700+Enteramine5@users.noreply.github.com>
2022-05-09 22:11:16 +0300

    Initial_Commit

---
+++ Summator/Summator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Summator
+{
+    public class Summator
+    {
+        public static long Sum(int[] arr)
+        {
+            long sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+            return sum;
+        }
+        
+        public static double Average(double[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+            return sum / arr.Length;
+        }
+    }
+}
